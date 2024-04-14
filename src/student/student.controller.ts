cbd23db42dcb29133b0aa6cecd2e753dc2cff90a
Pejoska99 <biljanapejoska985@gmail.com>
2024-04-14 11:44:27 +0200

    add commit

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { StudentService } from './student.service';
 import { CreateStudentDto } from './dto/create-student.dto';
 import { UpdateStudentDto } from './dto/update-student.dto';
+import { StudentDto } from './dto/student.dto';
+
+@UsePipes(
+  new ValidationPipe({
+    whitelist: true, 
+    forbidNonWhitelisted: false,
+    transform: true
+  })
+)
 
 @Controller('student')
 export class StudentController {
@@ -13,22 +22,22 @@ export class StudentController {
   }
 
   @Get()
-  findAll() {
-    return this.studentService.findAll();
+  findAll(@Query('age') age?: string):StudentDto[] {
+    return this.studentService.findAll(age);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.studentService.findOne(+id);
+  findOne(@Param('id') id: string): StudentDto {
+    return this.studentService.findOne(id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateStudentDto: UpdateStudentDto) {
-    return this.studentService.update(+id, updateStudentDto);
+    return this.studentService.update(id, updateStudentDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.studentService.remove(+id);
+  remove(@Param('id') id: string):boolean {
+    return this.studentService.remove(id);
   }
 }
